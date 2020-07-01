9875f38750035f4f304dcf05757560ed179b844a
KelseyCortez <kelscortez@gmail.com>
2020-07-01 14:12:00 -0400

    data is formatting

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { db } from './firebase';
 import ReactMarkdown from 'react-markdown';
 import Home from './Home';
+import { Card, Button, Container } from 'react-bootstrap';
 
 export default class Articles extends Component {
     constructor() {
@@ -19,7 +20,12 @@ export default class Articles extends Component {
             .then((querySnapshot) => {
                 let newArticles = [];
                 querySnapshot.forEach(doc => {
-                    newArticles.push(doc.data());
+                    // console.log(doc.data().content);
+                    let articleString = doc.data().content.replace(/\\n/g, '<br><br />') 
+                    articleString = articleString.replace(/\\/g, " ")
+                    console.log(articleString);
+                    const newObj = {...doc.data(), content: articleString}
+                    newArticles.push(newObj);
 
                 })
                 this.setState({
@@ -35,10 +41,17 @@ export default class Articles extends Component {
                 <Home />
                 {this.state.articles.map(article => {
                     return <div>
-                        {article.title}
-                        <ReactMarkdown source={article.content} />
+                        <Container >
+                        <Card className= "bg-dark text-white" >
+                            <Card.Header as="h5">{article.title}</Card.Header>
+                            <Card.Body>
+                                <Card.Title>Special title treatment</Card.Title>
+                                <Card.Text dangerouslySetInnerHTML={{__html: article.content}}/>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
 
-                        <br />
+                        </Card>
+                        </Container>
                     </div>
                 })}
             </div>
