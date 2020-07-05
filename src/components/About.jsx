b4272405e3714d235260b68f94e947ffdc4abfa1
KelseyCortez <kelscortez@gmail.com>
2020-07-05 15:07:16 -0400

    adds styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Card, Container } from 'react-bootstrap';
 
+
 export default class About extends Component {
     render() {
         return (
@@ -10,15 +11,14 @@ export default class About extends Component {
                         <Card.Title>Welcome To Press(Ed)</Card.Title>
                         <Card.Subtitle >About</Card.Subtitle>
                         <Card.Text>
-                            Press(Ed) is a resource for articles and historical information that aren't as widely known to be more accessible. By bridging this gap with knowledge, the goal is to have more emapthy, compassion, and understanding for all underrepresented groups.
+                            Press(Ed) is an educational resource that provides accessibility to historical information that isn't as widely known. By bridging this gap with knowledge, the goal is to have more empathy, compassion, and understanding for all underrepresented groups.
 
 
-                            If you would like to donate or want to hear about specific topics, feel free to donate through Charity Navigator and Put an idea in the suggestion box.
+                            If you would like to donate or want to hear about specific topics, feel free to donate through Charity Navigator and put an idea in the suggestion box.
 
                         
     </Card.Text>
-                        <Card.Link href="#">Card Link</Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
+                        
                     </Card.Body>
                 </Card>
             </Container>
