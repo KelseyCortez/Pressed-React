0fa6c8a887cba8f67cc1e0c09e55d1a104d6314f
KelseyCortez <kelscortez@gmail.com>
2020-07-06 10:49:05 -0400

    build

---
+++ build/precache-manifest.d4d1ef91a8503f8508547346feee098f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073f6435d4ecdf5a551d177233e71030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a854427b251d91000ef",
+    "url": "/static/css/main.84952297.chunk.css"
+  },
+  {
+    "revision": "e50f1e521aaeec5d31af",
+    "url": "/static/js/2.a1bb819e.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/static/js/2.a1bb819e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a854427b251d91000ef",
+    "url": "/static/js/main.d23c7006.chunk.js"
+  },
+  {
+    "revision": "8726e7655b17ea056a84",
+    "url": "/static/js/runtime-main.938f110f.js"
+  },
+  {
+    "revision": "481d2bc26107ab5e132dcee50880c393",
+    "url": "/static/media/faces.481d2bc2.png"
+  }
+]);
